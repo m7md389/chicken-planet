53696480024e0cb8f6d62dbb8b1fc0746deabac9
Mohammad <m7md389@gmail.com>
2022-02-15 22:26:47 +0200

    init

---
+++ services/httpService.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3001/api"
+});
+
+instance.interceptors.response.use(()=>{}, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    console.log(error);
+    toast.error("An unexpected error occurred.");
+  }
+
+  return Promise.reject(error);
+});
+
+const setToken = (token:string) => {
+  instance.defaults.headers.common["x-auth-token"] = token;
+};
+
+export default {
+  post: instance.post,
+  get: instance.get,
+  put: instance.put,
+  delete: instance.delete,
+  setToken
+};
