4cc0c793f36eb811d62afcc7b69ca0042dfb36b0
Mohammad <m7md389@gmail.com>
2022-02-24 14:30:56 +0200

    'server'

---
+++ pages/chickens/[name].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const ChickenDetails: NextPage = () => {
+  return (
+    <div>
+      <h2>Page is not ready yet</h2>
+    </div>
+  );
+};
+
+export default ChickenDetails;
